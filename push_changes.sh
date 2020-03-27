a9575a6151f01d76a1a114d7ff03048b06fb59c8
RunLittleFranz <f.sgherzi@urbantree.it>
2020-03-27 10:40:30 +0100

    Delete push_changes.sh

--- push_changes.sh
+++
@@ -1,2 +0,0 @@
-scp -i ../amazserver.pem -r ../LGAMEBOT ubuntu@ec2-18-219-108-60.us-east-2.compute.amazonaws.com:
-ssh -i ../amazserver.pem ubuntu@ec2-18-219-108-60.us-east-2.compute.amazonaws.com 'cd LGAMEBOT'
